2818a3486b45e3159157222a4291255c7bea2c20
Yaroslav Pugach <pugach.yaroslav2@gmail.com>
2019-10-15 21:23:40 +0300

    feat: Implemented data loading

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+import os
 import torch
 import checkpoint
 #import problem_unittests as tests
+
+
+def load_data(path):
+    """
+    Load Dataset from the file specified.
+    """
+    input_file = os.path.join(path)
+    with open(input_file, "r") as f:
+        data = f.read()
+
+    return data
+
 
 try:
     _, vocab_to_int, int_to_vocab, token_dict = checkpoint.load_preprocess()
@@ -8,4 +21,3 @@ try:
 except:
     data_dir = './data/Seinfeld_Scripts.txt'
     text = load_data(data_dir)
-    
